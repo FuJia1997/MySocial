299d37dc54ff5740d8c84b6049e388b83a1d180d
FuJia1997 <1390864739@qq.com>
2020-01-09 11:52:08 +0800

    创建MySocial项目

---
+++ MySocial/MySocial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mysocial
+//
+//  Created by yons on 2020/1/9.
+//  Copyright © 2020年 FuJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
